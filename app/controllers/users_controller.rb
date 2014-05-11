7fa317d2627d412e56ece31eadabd733cfcdb510
huydx <doxuanhuy@gmail.com>
2014-05-11 21:46:35 +0900

    Fix some bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,16 @@ class UsersController < ApplicationController
   protect_from_forgery :except => [:create]
 
   def create
-    user = User.find_by_name(verified_user_params[:name])
-    if user
-      user.generate_token && user.save
+    @user = User.find_by_name(verified_user_params[:name])
+    if @user
+      @user.generate_token && @user.save
       render_user_json_success and return
     else
-      user = User.create(verified_user_params)
-      if user.errors
-        render_user_json_failed and return
-      else
+      @user = User.create(verified_user_params)
+      if @user.errors.messages.empty?
         render_user_json_success and return
+      else
+        render_user_json_failed and return
       end
     end
   rescue Exception => e
@@ -21,11 +21,11 @@ class UsersController < ApplicationController
 
   private
   def render_user_json_failed
-    render json: {status: "failed", data: {message: u.errors.messages.as_json}}
+    render json: {status: "failed", data: {message: @user.errors.messages.as_json}}
   end
 
   def render_user_json_success
-    render json: {status: "success", data: {id: u.id, name: u.name, token: u.token, provider: u.provider}}
+    render json: {status: "success", data: {id: @user.id, name: @user.name, token: @user.token, provider: @user.provider}}
   end
   
   def verified_user_params
