7667a9f6702b43d38add5220805464532168480e
dxhuy1988 <doxuanhuy@gmail.com>
2014-05-31 00:57:19 +0900

    Remove act_as_superclass implement

--- app/models/question_review.rb
+++ app/models/question_review.rb
@@ -1,11 +1,3 @@
 class QuestionReview < ActiveRecord::Base
-  acts_as :question
-  attr_accessible :answer_reviews_attributes
-  has_many :answer_reviews, dependent: :destroy
-  after_save :skip_validation
-
-  protected
-  def should_validate_question
-    false
-  end
+  
 end
