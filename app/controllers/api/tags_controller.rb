c3ba8a178486c28acf94e4fc64e5a0b9d7035766
huydx <doxuanhuy@gmail.com>
2014-06-09 13:50:32 +0900

    Add full_image_path to tags api

--- app/controllers/api/tags_controller.rb
+++ app/controllers/api/tags_controller.rb
@@ -5,6 +5,7 @@ class Api::TagsController < Api::ApplicationController
     recent_db_update  = RecentlyUpdateTag.maximum(:updated_at)
     
     @tags = Tag.updated_after(asking_date)
+    @tags = @tags.each { |t| t["full_image_path"] = root_url + t.image.url }
     render json: {status: true, data: @tags.as_json(except: [:created_at, :updated_at])}
   rescue Exception => e
     logger.error(e.message)
